fa13d7820166b24ec948304b4f49ff9a9718575b
Wesley Max <48810791+Wesley-Max@users.noreply.github.com>
2019-08-29 15:50:14 -0300

    Update webpack.config.js, add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports  = {
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader'
+			}
+		]
 	}
 }
 	
